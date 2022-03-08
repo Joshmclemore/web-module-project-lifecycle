89569cc354e170f0efb8e79922c3b2125b7d06bb
Joshmclemore <josh_mclemore@yahoo.com>
2022-03-08 15:36:46 -0600

    built a mock DOM of what the app should look like

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -2,8 +2,34 @@ import React from 'react'
 
 const URL = 'http://localhost:9000/api/todos'
 
+const initialState =      {
+  id: "",
+  name: "",
+  completed: false,
+  form: {
+    textInput: ""
+  },
+  successMessage: "",
+  errorMessage: "",
+}
+
 export default class App extends React.Component {
+  state = initialState
   render() {
-    return null
+    return(
+      <div>
+        <h2>Todos:</h2>
+        <ul>
+          <li>Walk the dog</li>
+          <li>Learn React</li>
+          <li>Have fun</li>
+        </ul>
+        <form>
+          <input/>
+          <button>Submit</button>
+          <button>Hide Completed</button>
+        </form>
+      </div>
+    )
   }
 }
